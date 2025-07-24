647665ad69431e4a5a1c6452f0f81fc63de1c875
AzkaLufthansa <azkalufthansa3@gmail.com>
2025-07-24 23:38:24 +0700

    refactor(home): move BottomSheetJoinClass widget to seperate file

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:study_o/utils/app_colors.dart';
 import 'package:study_o/utils/dimens.dart';
+import 'package:study_o/widgets/bottom_sheet_join_class.dart';
 
 import '../widgets/home/my_classes_section.dart';
 
@@ -35,90 +36,6 @@ class HomePage extends StatelessWidget {
         },
         child: Icon(Icons.add),
         backgroundColor: const Color.fromARGB(255, 105, 1, 114),
-      ),
-    );
-  }
-}
-
-class BottomSheetJoinClass extends StatelessWidget {
-  const BottomSheetJoinClass({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(AppDimens.radiusLarge)
-        )
-      ),
-      width: double.infinity,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(AppDimens.radiusLarge),
-              topRight: Radius.circular(AppDimens.radiusLarge),
-            ),
-              onTap: () {},
-              child: SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: AppDimens.marginPaddingExtraLarge,
-                    bottom: AppDimens.marginPaddingLarge,
-                  ),
-                  child: Text(
-                    'Create Class',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Divider(height: 1,),
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () {},
-              child: SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: AppDimens.marginPaddingExtraLarge,
-                    bottom: AppDimens.marginPaddingLarge,
-                  ),
-                  child: Text(
-                    'Join Public Class',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Divider(height: 1,),
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () {},
-              child: SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: AppDimens.marginPaddingLarge,
-                    bottom: AppDimens.marginPaddingExtraLarge,
-                  ),
-                  child: Text(
-                    'Join Private Class',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/widgets/bottom_sheet_join_class.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../utils/dimens.dart';
+
+class BottomSheetJoinClass extends StatelessWidget {
+  const BottomSheetJoinClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(AppDimens.radiusLarge)
+        )
+      ),
+      width: double.infinity,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(AppDimens.radiusLarge),
+              topRight: Radius.circular(AppDimens.radiusLarge),
+            ),
+              onTap: () {},
+              child: SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: AppDimens.marginPaddingExtraLarge,
+                    bottom: AppDimens.marginPaddingLarge,
+                  ),
+                  child: Text(
+                    'Create Class',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Divider(height: 1,),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {},
+              child: SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: AppDimens.marginPaddingExtraLarge,
+                    bottom: AppDimens.marginPaddingLarge,
+                  ),
+                  child: Text(
+                    'Join Public Class',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Divider(height: 1,),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {},
+              child: SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: AppDimens.marginPaddingLarge,
+                    bottom: AppDimens.marginPaddingExtraLarge,
+                  ),
+                  child: Text(
+                    'Join Private Class',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
