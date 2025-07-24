c78be3d6f710ba3cb37e460328be33ec1517eadd
arizalmaulana <arizalmaulana602@gmail.com>
2025-07-24 23:22:12 +0700

    feat: add classroom for see more

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:study_o/screens/flashcard_page.dart';
 import 'package:study_o/screens/login_page.dart';
 import 'package:study_o/screens/material.dart';
 
+import '../screens/classroom.dart';
 import '../screens/home_page.dart';
 import '../screens/profile_page.dart';
 import '../screens/quiz.dart';
@@ -24,6 +25,7 @@ class AppRoutes {
       page: () => ProfilePage()
     ),
     GetPage(name: '/classroom_dashboard', page: () => ClassroomDashboard()),
+    GetPage(name: '/classroom', page: () => Classroom()),
     GetPage(name: '/add_material', page: () => AddMaterialPage()),
     GetPage(name: '/quiz', page: () => Quiz()),
     GetPage(name: '/material', page: () => Material()),
